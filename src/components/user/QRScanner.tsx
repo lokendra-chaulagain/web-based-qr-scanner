2e75e60cd31cf5158fff49d74d5fa1dd1a8348ed
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2023-05-11 06:34:13 +0545

    access the camera only after permission

--- src/components/user/QRScanner.tsx
+++ src/components/user/QRScanner.tsx
@@ -1,23 +1,54 @@
+// import { BrowserMultiFormatReader } from "@zxing/library";
+// import { useEffect, useRef, useState } from "react";
+
+// export default function QRScanner() {
+//   const videoRef = useRef<any>(null);
+//   // const [code, setCode] = useState(null);
+//   const [code, setCode] = useState<string | null>(null);
+
+//   useEffect(() => {
+//     const codeReader = new BrowserMultiFormatReader();
+//     codeReader.decodeFromVideoDevice(null, videoRef.current?.id, (result, err) => {
+//       if (result) {
+//         // setCode(result.text);
+//         setCode(result.getText());
+//       }
+//       if (err) {
+//         console.error(err);
+//       }
+//     });
+//   }, []);
+
+//   return (
+//     <div>
+//       <video
+//         ref={videoRef}
+//         id="video"
+//         style={{ width: "100%", maxWidth: 640 }}
+//       />
+//       {code && <p>{code}</p>}
+//     </div>
+//   );
+// }
+
 import { BrowserMultiFormatReader } from "@zxing/library";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 export default function QRScanner() {
   const videoRef = useRef<any>(null);
-  // const [code, setCode] = useState(null);
   const [code, setCode] = useState<string | null>(null);
 
-  useEffect(() => {
+  const startScanning = () => {
     const codeReader = new BrowserMultiFormatReader();
     codeReader.decodeFromVideoDevice(null, videoRef.current?.id, (result, err) => {
       if (result) {
-        // setCode(result.text);
         setCode(result.getText());
       }
       if (err) {
         console.error(err);
       }
     });
-  }, []);
+  };
 
   return (
     <div>
@@ -27,6 +58,13 @@ export default function QRScanner() {
         style={{ width: "100%", maxWidth: 640 }}
       />
       {code && <p>{code}</p>}
+      <div className="flex justify-center items-center">
+      <p
+        onClick={startScanning}
+        className=" w-32 cursor-pointer  py-2 px-5 text-base font-medium text-white hover:bg-opacity-90  bg-primary-600 rounded-lg">
+      Scan QR
+      </p>
+      </div>
     </div>
   );
 }
