01ffaae4f1f92745c8077688509dcae95c19e477
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2023-05-11 07:02:13 +0545

    final

--- src/components/user/QRScanner.tsx
+++ src/components/user/QRScanner.tsx
@@ -1,92 +1,17 @@
-// import { BrowserMultiFormatReader } from "@zxing/library";
-// import { useEffect, useRef, useState } from "react";
-
-// export default function QRScanner() {
-//   const videoRef = useRef<any>(null);
-//   // const [code, setCode] = useState(null);
-//   const [code, setCode] = useState<string | null>(null);
-
-//   useEffect(() => {
-//     const codeReader = new BrowserMultiFormatReader();
-//     codeReader.decodeFromVideoDevice(null, videoRef.current?.id, (result, err) => {
-//       if (result) {
-//         // setCode(result.text);
-//         setCode(result.getText());
-//       }
-//       if (err) {
-//         console.error(err);
-//       }
-//     });
-//   }, []);
-
-//   return (
-//     <div>
-//       <video
-//         ref={videoRef}
-//         id="video"
-//         style={{ width: "100%", maxWidth: 640 }}
-//       />
-//       {code && <p>{code}</p>}
-//     </div>
-//   );
-// }
-
-// import { BrowserMultiFormatReader } from "@zxing/library";
-// import { useRef, useState } from "react";
-
-// export default function QRScanner() {
-//   const videoRef = useRef<any>(null);
-//   const [code, setCode] = useState<string | null>(null);
-
-//   const startScanning = () => {
-//     const codeReader = new BrowserMultiFormatReader();
-//     codeReader.decodeFromVideoDevice(null, videoRef.current?.id, (result, err) => {
-//       if (result) {
-//         setCode(result.getText());
-//       }
-//       if (err) {
-//         console.error(err);
-//       }
-//     });
-//   };
-
-//   return (
-//     <div>
-//       <video
-//         ref={videoRef}
-//         id="video"
-//         style={{ width: "100%", maxWidth: 640 }}
-//       />
-//       {code && <p>{code}</p>}
-//       <div className="flex justify-center items-center">
-//       <p
-//         onClick={startScanning}
-//         className=" w-32 cursor-pointer  py-2 px-5 text-base font-medium text-white hover:bg-opacity-90  bg-primary-600 rounded-lg">
-//       Scan QR
-//       </p>
-//       </div>
-//     </div>
-//   );
-// }
-
-
 import { BrowserMultiFormatReader } from "@zxing/library";
 import { useRef, useState } from "react";
 
 export default function QRScanner() {
   const videoRef = useRef<any>(null);
   const [code, setCode] = useState<string | null>(null);
+  const [visible, setVisible] = useState<boolean>(false);
 
   const startScanning = () => {
+    setVisible(true);
     const codeReader = new BrowserMultiFormatReader();
     codeReader.decodeFromVideoDevice(null, videoRef.current?.id, (result, err) => {
       if (result) {
-        const text = result.getText();
-        const formattedText = text
-          .split("\n")
-          .map((line, index) => `${index + 1}. ${line}`)
-          .join("\n");
-        setCode(formattedText);
+        setCode(result.getText());
       }
       if (err) {
         console.error(err);
@@ -94,22 +19,37 @@ export default function QRScanner() {
     });
   };
 
+  const closeScanner = () => {
+    setVisible(false);
+  };
+  console.log(visible);
+
   return (
     <div>
       <video
         ref={videoRef}
         id="video"
-        style={{ width: "100%", maxWidth: 640 }}
+        style={{ width: "100%", maxWidth: 640, display: visible ? "block" : "none" }}
       />
       {code && <p>{code}</p>}
-      <div className="flex justify-center items-center">
+
+      {!visible && <div className="flex justify-center items-center">
         <p
           onClick={startScanning}
-          className="w-32 cursor-pointer py-2 px-5 text-base font-medium text-white hover:bg-opacity-90 bg-primary-600 rounded-lg"
-        >
+          className=" w-32 cursor-pointer  py-2 px-5 text-base font-medium text-white hover:bg-opacity-90  bg-primary-600 rounded-lg">
           Scan QR
         </p>
-      </div>
+      </div>}
+
+      {visible && (
+        <div className="flex justify-center items-center mt-4">
+          <p
+            onClick={closeScanner}
+            className=" w-32 cursor-pointer  py-2 px-5 text-base font-medium text-white hover:bg-opacity-90  bg-red-600 rounded-lg">
+            Close
+          </p>
+        </div>
+      )}
     </div>
   );
 }
