a9e8935757c7e021cd94aaae35a4d6d7df2ab024
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2023-05-11 06:42:27 +0545

    clean

--- src/components/user/QRScanner.tsx
+++ src/components/user/QRScanner.tsx
@@ -31,6 +31,45 @@
 //   );
 // }
 
+// import { BrowserMultiFormatReader } from "@zxing/library";
+// import { useRef, useState } from "react";
+
+// export default function QRScanner() {
+//   const videoRef = useRef<any>(null);
+//   const [code, setCode] = useState<string | null>(null);
+
+//   const startScanning = () => {
+//     const codeReader = new BrowserMultiFormatReader();
+//     codeReader.decodeFromVideoDevice(null, videoRef.current?.id, (result, err) => {
+//       if (result) {
+//         setCode(result.getText());
+//       }
+//       if (err) {
+//         console.error(err);
+//       }
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <video
+//         ref={videoRef}
+//         id="video"
+//         style={{ width: "100%", maxWidth: 640 }}
+//       />
+//       {code && <p>{code}</p>}
+//       <div className="flex justify-center items-center">
+//       <p
+//         onClick={startScanning}
+//         className=" w-32 cursor-pointer  py-2 px-5 text-base font-medium text-white hover:bg-opacity-90  bg-primary-600 rounded-lg">
+//       Scan QR
+//       </p>
+//       </div>
+//     </div>
+//   );
+// }
+
+
 import { BrowserMultiFormatReader } from "@zxing/library";
 import { useRef, useState } from "react";
 
@@ -42,7 +81,12 @@ export default function QRScanner() {
     const codeReader = new BrowserMultiFormatReader();
     codeReader.decodeFromVideoDevice(null, videoRef.current?.id, (result, err) => {
       if (result) {
-        setCode(result.getText());
+        const text = result.getText();
+        const formattedText = text
+          .split("\n")
+          .map((line, index) => `${index + 1}. ${line}`)
+          .join("\n");
+        setCode(formattedText);
       }
       if (err) {
         console.error(err);
@@ -59,11 +103,12 @@ export default function QRScanner() {
       />
       {code && <p>{code}</p>}
       <div className="flex justify-center items-center">
-      <p
-        onClick={startScanning}
-        className=" w-32 cursor-pointer  py-2 px-5 text-base font-medium text-white hover:bg-opacity-90  bg-primary-600 rounded-lg">
-      Scan QR
-      </p>
+        <p
+          onClick={startScanning}
+          className="w-32 cursor-pointer py-2 px-5 text-base font-medium text-white hover:bg-opacity-90 bg-primary-600 rounded-lg"
+        >
+          Scan QR
+        </p>
       </div>
     </div>
   );
