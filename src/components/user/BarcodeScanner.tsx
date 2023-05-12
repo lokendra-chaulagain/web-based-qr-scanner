966e480ea1c28174ea712141c6dadd2c7856cf5e
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2023-05-12 12:26:12 +0545

    /

--- src/components/user/BarcodeScanner.tsx
+++ src/components/user/BarcodeScanner.tsx
@@ -1,3 +1,19 @@
+import { useEffect } from "react";
+
 export default function BarcodeScanner() {
-  return <div></div>;
+  useEffect(() => {
+    // check compatibility
+    if (!("BarcodeDetector" in window)) {
+      console.log("Barcode Detector is not supported by this browser.");
+    } else {
+      console.log("Barcode Detector supported!");
+
+      // // create new detector
+      // const barcodeDetector = new BarcodeDetector({
+      //   formats: ["code_39", "codabar", "ean_13"],
+      // });
+    }
+  }, []);
+
+  return <div>ssssss</div>;
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,17 @@
 import { lazy, Suspense } from "react";
 import QRScanner from "../components/user/QRScanner";
-import BarcodeScanner from "../components/user/BarcodeScanner";
-const Features = lazy(() => import("../components/user/Features"));
-const HeroSection = lazy(() => import("../components/user/HeroSection"));
+// import BarcodeScanner from "../components/user/BarcodeScanner";
+// const Features = lazy(() => import("../components/user/Features"));
+// const HeroSection = lazy(() => import("../components/user/HeroSection"));
 
 export default function Home() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <HeroSection />
+      {/* <HeroSection /> */}
       <QRScanner/>
 
-      <BarcodeScanner/>
-      <Features />
+      {/* <BarcodeScanner/> */}
+      {/* <Features /> */}
     </Suspense>
   );
 }
