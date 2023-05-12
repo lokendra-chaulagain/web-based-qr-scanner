277502ea9bb9cffcc9da2feddea51780d38e02f5
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2023-05-12 12:27:41 +0545

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from "react";
+import {  Suspense } from "react";
 import QRScanner from "../components/user/QRScanner";
 // import BarcodeScanner from "../components/user/BarcodeScanner";
 // const Features = lazy(() => import("../components/user/Features"));
